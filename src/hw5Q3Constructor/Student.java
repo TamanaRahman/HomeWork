4b1bcd80eddab3e453a4152f032ece5605a08dd3
TamanaRahman <tmnnarhmn@gmail.com>
2022-01-29 19:36:51 -0500

    01292022 7:36pm

--- src/hw5Q3Constructor/Student.java
+++ src/hw5Q3Constructor/Student.java
@@ -1,9 +1,25 @@
 package hw5Q3Constructor;
 
 public class Student {
+	public String stName;
+	public int stID;
+	public char sex;
+	public float grade;
+	public boolean isProgrammer;
 
-	public 
-	
+	public Student() {
+		System.out.println("This is form default Constructor of student class");
+	}
+
+	public Student(String stName, int stID, char sex, float grade, boolean isProgrammer) {
+		this.stName = stName;
+		this.stID = stID;
+		this.sex = sex;
+		this.grade = grade;
+		this.isProgrammer =isProgrammer;
+		
+		System.out.println("Student Name :" + stName + " ID :" + stID + " Sex :" + sex + " Grade:" + grade
+				+ " Java programmer? Ans:" + isProgrammer);
 
 	}
 
---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,9 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
+		Student studenyt1 = new Student("Tamanna Rahman", 345, 'F', 3.976f, true);
+
+	}
+}
